8eb105dabf2f7cd012eb16da4d007c2fdd37f180
Rishi Patni <rishipatni795@gmail.com>
2026-07-11 19:37:47 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (70.11%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,29 @@
+class Solution {
+    TreeNode first, second, prev;
+
+    public void recoverTree(TreeNode root) {
+        prev = new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
+
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+
+        inorder(root.left);
+
+        if (prev.val > root.val) {
+            if (first == null) {
+                first = prev;
+            }
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
+    }
+}
